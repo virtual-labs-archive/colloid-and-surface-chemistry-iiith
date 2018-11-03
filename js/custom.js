1fbfe796715f4b0c9ede2505f7f0ca93a579acb7
sahithireddy27 <43744746+sahithireddy27@users.noreply.github.com>
2018-11-03 20:56:48 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -28,8 +28,8 @@ $(function() {
 
 // Highlight the top nav as scrolling occurs
 $("body").scrollspy({
-    target: ".navbar-fixed-top";
-})
+    target: ".navbar-fixed-top"
+});
 
 // Closes the Responsive Menu on Menu Item Click
 $(".navbar-collapse ul li a").click(function() {
