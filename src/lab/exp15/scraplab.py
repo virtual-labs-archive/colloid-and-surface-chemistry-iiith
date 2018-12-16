6f1ee481a5b329e5f3efe0b03ee59c880862ac9d
Somashekhar31 <17311a05r5@sreenidhi.edu.in>
2018-12-16 12:21:19 +0530

    resolve s3

--- src/lab/exp15/scraplab.py
+++ src/lab/exp15/scraplab.py
@@ -1,19 +1,17 @@
 from bs4 import BeautifulSoup
-heading=""
-breadcrumb=""
 def writefile(fname,s,s1,heading):
 	f=open(fname, 'w+')
 	f.write(template)
 	f.seek(0)
+	
 	content = f.read()
-	content=content.replace('Disciplines and Domains',breadcrumb)
+	content=content.replace('Disciplines and Domains',heading)
 	f.seek(0)
 	f.write(content)
 	print content
 	
 	f.seek(0)
 	content=f.read()
-	k=content.index('<div class="col-md-10 lab-list-col-10">')
 	
 	t1=content.index('<!--edit1-->')
 	print t1
@@ -26,7 +24,6 @@ def writefile(fname,s,s1,heading):
 	content=f.read()
 	t= content.index('<!--edit -->')
 	f.seek(t+13)
-	s='<h1 class="text-h2-lightblue">'+heading+'</h1>'+s
 	s=s+f.read()
 	f.seek(t+13)
 	f.write(s)
@@ -41,54 +38,51 @@ d=['introduction','theory','objective','experiment','manual','quizzes','further_
 print len(sectionno)
 sectionNumber=1
 st=""
-
-att = ''+'lab-article-heading'
-tagger = soup.findAll('div', attrs={'id':att,'class':'heading'})
+att = ''+'experiment-article-heading'
+tagger = soup.findAll('header', attrs={'id':att,'class':'heading'})
 heading=str(tagger[0].text)
 heading=heading.strip()
 print heading
-att = ''+'lab-header-heading'
-tagger = soup.findAll('div', attrs={'id':att,'class':'heading'})
-heading1=str(tagger[0].text)
-print heading
-heading1=heading1.strip()
-breadcrumb='<a href="http://vlabs.ac.in/chemical-sciences-labs.html" class="sidebar-a" >Chemical Sciences</a> &rarr;<a href="../Introduction.html" class="sidebar-a" >'+heading1+'</a>&nbsp&rarr;<a href="../Experiments.html" class="sidebar-a" >List Of Experiments</a><br/>'
 while sectionNumber<=len(sectionno):
 	tag=""
-	att = ''+'lab-article-section-'+str(sectionNumber)+'-heading'
+	att = ''+'experiment-article-section-'+str(sectionNumber)+'-heading'
 	tagger = soup.findAll('div', attrs={'id':att,'class':'heading'})
 	if not tagger:
 		sectionNumber=sectionNumber+1
 		continue
-tag1 = str(tagger[0].text)
-tag1 = tag1.strip()
-print tag1
-if tag1 == 'Prerequisite S/W':
-    tag1 = 'Prerequisites'
-st += '<a href="'+tag1+'.html?domain=Chemical Sciences&lab='+heading1+'" class="sidebar-a" > <h3 class="text-h3-darkblue" style="margin-top: 2px;">'+tag1+'</h3></a>'	
-sectionNumber = sectionNumber+1
-st += '<a href="Feedback.html?domain=Chemical Sciences&lab='+heading1+'" class="sidebar-a" > <h3 class="text-h3-darkblue" style="margin-top: 2px;">Feedback</h3></a>'
-sectionNumber = 1
+	tag1=str(tagger[0].text)
+	tag1=tag1.strip()
+	print tag1
+	if tag1=='Prerequisite S/W':
+		tag1='Prerequisites'
+	st+='<a href="'+tag1+'.html"'+'class="sidebar-a" > <h3 class="text-h3-darkblue" style="margin-top: 2px; color: #ff6600;">'+tag1+'</h3></a>'	
+	sectionNumber=sectionNumber+1
+sectionNumber=1
 while sectionNumber<=len(sectionno):
 	tag=""
 	tag=""
-	att = ''+'lab-article-section-'+str(sectionNumber)+'-heading'
+	att = ''+'experiment-article-section-'+str(sectionNumber)+'-heading'
 	tagger = soup.findAll('div', attrs={'id':att,'class':'heading'})
 	if not tagger:
 		sectionNumber=sectionNumber+1
 		continue
-tag1 = str(tagger[0].text)	
-tag1 = tag1.strip() 
-if tag1 == 'Prerequisite S/W':
-    tag1 = 'Prerequisites'
-att = ''+'lab-article-section-'+str(sectionNumber)+'-content'
-tagger = soup.findAll('div', attrs={'id': att, 'class': 'content'})
-tag += str(tagger[0])
-#print tag
-#print st
-writefile(tag1+'.html',tag,st,heading)
-#print sectionNumber
-sectionNumber = sectionNumber+1
+	tag1=str(tagger[0].text)	
+	tag1=tag1.strip()
+	if tag1=='Prerequisite S/W':
+		tag1='Prerequisites'
+	att = ''+'experiment-article-section-'+str(sectionNumber)+'-content'
+	tagger = soup.findAll('div', attrs={'id':att,'class':'content'})
+	
+	tag+=str(tagger[0])
+	#print tag
+	#print st
+	
+	
+	writefile(tag1+'.html',tag,st,heading)
+	#print sectionNumber
+	sectionNumber=sectionNumber+1
+	
+	
 f = open("Feedback.html",'w+')
 f.write(template)
 f.seek(0)
