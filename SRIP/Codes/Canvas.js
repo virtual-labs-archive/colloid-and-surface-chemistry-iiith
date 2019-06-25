8cbc4d9f5b79625bbf8055ac38c2d6f79a25fad1
Ratnesh Dubey <blazerd18@gmail.com>
2019-06-25 19:33:50 +0530

    more code added in canvas.js

--- SRIP/Codes/Canvas.js
+++ SRIP/Codes/Canvas.js
@@ -51,13 +51,13 @@ clear_image(intr);
 //clearScreen();
 make_base('(86).svg');
 
-canvas.addEventListener('click',coords,{once: true});
+canvas.addEventListener('click',coords,{once:true});
 function coords()
 {
     cx = event.pageX;
     cy = event.pageY; // or event.pageY
 
-    //alert('x,y = '+ cx + ','+ cy);
+    alert('x,y = '+ cx + ','+ cy);
 
     var cx_arr = [];
     var cy_arr = [];
@@ -355,7 +355,7 @@ function coords6(event)
 
     //canvas.removeEventListener('click', coords4);
 	}
-canvas.addEventListener('click',coords7,{once:false});
+canvas.addEventListener('click',coords7,{once:true});
 function coords7(event)
 {
 
@@ -382,14 +382,136 @@ function coords7(event)
 
 	if(n&&z ==true) // new block start
 	{
-	var h = 151;
+	var h = 150;
 	var intr2 = setInterval(function()
 		{
 
 	var add2 = 'new_frames/'+'('+h+')'+'.svg';
 	make_base(add2);
 
-	if(h>156){
+	if(h>155){
+
+	 clear_image(intr2);
+
+	}
+	else{ h+=1;}
+
+	},delayInMilliseconds);
+
+    //canvas.removeEventListener('click', coords4);
+	}
+
+canvas.addEventListener('click',coords8,{once:true});
+function coords8(event)
+{
+
+	cx = event.pageX;
+    cy = event.pageY;
+    //alert('x,y = '+ cx + ','+ cy);
+
+    var cx_arr = [];
+    var cy_arr = [];
+    for(var i=347;i<=446;i++)
+	{
+	cx_arr.push(i);
+	}
+
+	for(i=467;i<=506;i++)
+	{
+
+	cy_arr.push(i);
+	}
+
+	var n = cx_arr.includes(cx);
+	var z = cy_arr.includes(cy);
+	
+
+	if(n&&z ==true) // new block start
+	{
+	var h = 156;
+	var intr2 = setInterval(function()
+		{
+
+	var add2 = 'new_frames/'+'('+h+')'+'.svg';
+	make_base(add2);
+
+	if(h>222){
+
+	 clear_image(intr2);
+
+	}
+	else{ h+=1;}
+
+	},delayInMilliseconds);
+
+    //canvas.removeEventListener('click', coords4);
+	}
+canvas.addEventListener('click',coords9,{once:true});
+function coords9(event)
+{
+
+	cx = event.pageX;
+    cy = event.pageY;
+    //alert('x,y = '+ cx + ','+ cy);
+
+    var cx_arr = [];
+    var cy_arr = [];
+    var cx_alt = [];
+    var cy_alt = [];
+    for(var i=1007;i<=1072;i++)
+	{
+	cx_arr.push(i);
+	}
+
+	for(i=530;i<=562;i++)
+	{
+
+	cy_arr.push(i);
+	}
+	for(var i=990;i<=1068;i++)
+	{
+		cx_alt.push(i);
+	}
+    for(var i=570;i<=591;i++)
+    {
+    	cy_alt.push(i);
+    }
+	var n = cx_arr.includes(cx);
+	var z = cy_arr.includes(cy);
+	var m = cx_alt.includes(cx);
+	var g = cy_alt.includes(cy);
+	
+
+	if(n&&z ==true ) // new block start
+	{
+	var h = 223;
+	var intr2 = setInterval(function()
+		{
+
+	var add2 = 'new_frames/'+'('+h+')'+'.svg';
+	make_base(add2);
+
+	if(h>231){
+
+	 clear_image(intr2);
+
+	}
+	else{ h+=1;}
+
+	},delayInMilliseconds);
+
+    //canvas.removeEventListener('click', coords4);
+	}
+	else if(m&&g ==true) // new block start
+	{
+	var h = 522;
+	var intr2 = setInterval(function()
+		{
+
+	var add2 = 'new_frames/'+'('+h+')'+'.svg';
+	make_base(add2);
+
+	if(h>534){
 
 	 clear_image(intr2);
 
@@ -403,6 +525,13 @@ function coords7(event)
 
 
 
+
+}
+
+
+
+
+}
 
 }
 
